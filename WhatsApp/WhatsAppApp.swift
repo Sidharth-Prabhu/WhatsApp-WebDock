ab92fad2b06dd691df8ccbd3378dcab70fbd1b6c
Sidharth Prabhu <cyberzypher@gmail.com>
2026-08-30 20:09:37 +0530

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Sidharth Prabhu on 2026-08-30.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhatsAppTests/WhatsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppTests.swift
+//  WhatsAppTests
+//
+//  Created by Sidharth Prabhu on 2026-08-30.
+//
+
+import Testing
+@testable import WhatsApp
+
+struct WhatsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
